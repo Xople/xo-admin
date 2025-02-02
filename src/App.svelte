8b64db001015f4c5cf4690774a276983f73e2395
Tito Shadam <tito@lingkar9.com>
2025-02-02 11:10:00 +0700

    INITIAL COMMIT

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center justify-center">
+      <!-- Page content here -->
+      <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
+        Open drawer
+      </label>
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+      <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count = $state<number>(0)
+  const increment = (): void => {
+    count += 1
+  }
+</script>
+
+<button class="btn btn-accent btn-outline" onclick={increment}>
+  count is {count}
+</button>
